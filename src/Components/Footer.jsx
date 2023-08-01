bc27dcb2f17c07dc1c781fd0df46f4eb249ded69
nimo <nimo.sheikh@student.moringaschool.com>
2023-08-01 14:56:01 +0300

    Added About, Contact and footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const Footer = () => {
+function Footer() {
+  const handleScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
-    <div>Footer</div>
-  )
+    <div className="bg-gradient-to-t from-red-900 to-red-700 py-12 text-white flex justify-center items-center">
+      <div className="flex flex-col space-y-4">
+        <NavLink
+          to="/"
+          exact
+          className="font-bold text-2xl md:text-3xl lg:text-4xl text-white hover:text-yellow-400 transition duration-300 flex items-center space-x-2 animate-bounce"
+        >
+          <i className="fas fa-home text-yellow-400 text-lg md:text-xl lg:text-2xl"></i>
+          Home
+        </NavLink>
+        <NavLink
+          className="font-bold text-2xl md:text-3xl lg:text-4xl text-white hover:text-yellow-400 transition duration-300 flex items-center space-x-2 animate-bounce"
+          to="/menu"
+        >
+          <i className="fas fa-book-open text-yellow-400 text-lg md:text-xl lg:text-2xl"></i>
+          Menu
+        </NavLink>
+        <NavLink
+          className="font-bold text-2xl md:text-3xl lg:text-4xl text-white hover:text-yellow-400 transition duration-300 flex items-center space-x-2 animate-bounce"
+          to="/contact"
+        >
+          <i className="fas fa-envelope text-yellow-400 text-lg md:text-xl lg:text-2xl"></i>
+          Contact Us
+        </NavLink>
+        {/* Add the "About Us" NavLink */}
+        <NavLink
+          className="font-bold text-2xl md:text-3xl lg:text-4xl text-white hover:text-yellow-400 transition duration-300 flex items-center space-x-2 animate-bounce"
+          to="/about"
+        >
+          <i className="fas fa-info-circle text-yellow-400 text-lg md:text-xl lg:text-2xl"></i>
+          About Us
+        </NavLink>
+        <p className="text-xs md:text-sm lg:text-base text-center">
+          © 2023 MunchHubEats Restaurant. All rights reserved.
+        </p>
+        <p className="text-xs md:text-sm lg:text-base text-center">
+          Address: 123 Main Street, City, Country
+        </p>
+        <p className="text-xs md:text-sm lg:text-base text-center">
+          Phone: +1 (123) 456-7890 | Email: info@MunchHubEatssrestaurant.com
+        </p>
+        <div className="flex items-center justify-center space-x-4">
+          <a
+            href="https://www.facebook.com/deliciousrestaurant"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white hover:text-yellow-400 transition duration-300"
+          >
+            <i className="fab fa-facebook text-xl md:text-2xl lg:text-3xl"></i>
+          </a>
+          <a
+            href="https://www.instagram.com/deliciousrestaurant"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white hover:text-yellow-400 transition duration-300"
+          >
+            <i className="fab fa-instagram text-xl md:text-2xl lg:text-3xl"></i>
+          </a>
+          <button
+            onClick={handleScrollToTop}
+            className="bg-yellow-400 hover:bg-yellow-300 text-black font-bold py-2 px-4 rounded-full transition duration-300"
+          >
+            Scroll to Top
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,100 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const Contact = () => {
+const ContactUs = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name);
+    console.log(email);
+    console.log(message);
+
+    // Send the contact information to the server
+    fetch('/contact', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name, email, message }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          alert('Message sent successfully');
+          return response.json();
+        }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        console.log('Response:', error.response);
+      });
+  };
+
   return (
-    <div>Contact</div>
-  )
-}
+    <div
+      className="flex items-center justify-center min-h-screen bg-gradient-to-r from-purple-500 to-pink-500"
+    >
+      <div className="w-full max-w-md px-4 py-8 bg-white rounded-md shadow-lg">
+        <h2 className="text-3xl font-semibold mb-6 text-center text-indigo-700">
+          Contact Us
+        </h2>
+        <p className="font-medium italic mb-4 text-center text-gray-700">
+          Feel free to get in touch with us if you have any questions, proposals,
+          or just want to say hello to our restaurant!
+        </p>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div className="flex space-x-4">
+            <div className="w-1/2">
+              <label className="block text-sm font-medium text-gray-700 w-full px-4 py-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300">
+                Name
+              </label>
+              <input
+                type="text"
+                className="mt-1 block w-full border rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm w-full px-4 py-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className="w-1/2">
+              <label className="block text-sm font-medium text-gray-700 w-full px-4 py-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300">
+                Email
+              </label>
+              <input
+                type="email"
+                className="mt-1 block w-full border rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm w-full px-4 py-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300 "
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700 w-full px-4 py-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300">
+              Your Message
+            </label>
+            <textarea
+              className="mt-1 block w-full border rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            ></textarea>
+          </div>
+          <button
+            className="w-full bg-indigo-600 text-white py-3 px-4 rounded hover:bg-indigo-700 transition-colors"
+            type="submit"
+          >
+            Send Message
+          </button>
+        </form>
+        <p className="mt-4 text-center text-gray-600">
+          If you have an account, <a className="text-indigo-700" href="/login">Login</a>
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default ContactUs;
