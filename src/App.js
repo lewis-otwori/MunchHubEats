264c9b9c9e97842fca85f9b248ba413ca058dea2
Byron Omondi <omondibyron25@gmail.com>
2023-08-01 12:12:15 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from "react";
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Layout from "./Layout/BaseLayout";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
 
-
-
 function App() {
-  const router = createBrowserRouter( 
-   createRoutesFromElements(
-    <Route path="/" elements = {<Layout/>}>
-    <Route index element= {<Home/>}/>
-    <Route path="/contact" elements = {<Contact/>}/>
-    <Route path="/about" elements = {<About/>}/>
-    </Route>
-   ) 
-  )
-  return (<RouterProvider router={router}/>)
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
