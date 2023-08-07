f839971ea4787a9fda011a4e1676577278ef4e92
nimo <nimo.sheikh@student.moringaschool.com>
2023-08-07 14:06:44 +0300

    updated footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ import {BiRestaurant} from "react-icons/bi"
 const Footer = () => {
   return (
     
-<footer class="bg-white dark:bg-gray-900">
+<footer class="bg-[#d2a297] dark:bg-gray-900">
     <div class="mx-auto w-full max-w-[1560px] p-4 py-6 lg:py-8">
         <div class="md:flex md:justify-between">
           <div class="mb-6 md:mb-0">
